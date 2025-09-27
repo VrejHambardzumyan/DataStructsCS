a4d1bfafa1ab4e55481ef49f638fcd3f2db7d212
VrejHambardzumyan <vrezhambardzumyan04@gmail.com>
2025-09-27 23:55:30 +0400

    More implementations of LinkedList

--- CustomList/MyCustomList.cs
+++ CustomList/MyCustomList.cs
@@ -1,4 +1,6 @@
-﻿namespace CustomList
+﻿using System;
+
+namespace MyCustomList
 {
     class CustomList<T>
     {
@@ -36,16 +38,16 @@
         public bool Contains(T item)
         {
             int i = 0;
-            if(_items == null)
+            if (_items == null)
                 return false;
 
             while (i < _items.Length && !_items[i].Equals(item))
-            { 
+            {
                 i++;
             }
-            if(i != _items.Length)
+            if (i != _items.Length)
                 return true;
-            
+
             return false;
         }
         public bool Remove(T item)
@@ -55,12 +57,11 @@
 
             int i = 0;
 
-            //karayi sra poxaren im impelemntac contains-y use anei
             while (i < _items.Length && !_items[i].Equals(item))
             {
                 i++;
             }
-            if(i == _items.Length)
+            if (i == _items.Length)
                 return false;
             var newLenght = _count - 1;
             var _newItems = new T[_items.Length];
@@ -71,12 +72,12 @@
             }
             for (int j = i; j < newLenght; j++)
             {
-                _newItems[j] = _items[j+1];
+                _newItems[j] = _items[j + 1];
             }
             _items = _newItems;
             _count--;
             return true;
-           
+
         }
 
         public void RemoveAt(int index)
@@ -123,22 +124,22 @@
                 }
                 _items = _newItems;
             }
-            var _newitems = new T[_items.Length]; 
+            var _newitems = new T[_items.Length];
             int k;
-            for ( k = 0; k < index; k++)
+            for (k = 0; k < index; k++)
             {
                 _newitems[k] = _items[k];
             }
-            
+
             _newitems[k++] = item;
-            
+
             for (int j = k; j < _items.Length; j++)
             {
                 _newitems[j] = _items[j - 1];
             }
             _items = _newitems;
         }
-       
+
         public void Print()
         {
             for (int i = 0; i < _items.Length; i++)
@@ -147,33 +148,6 @@
             }
         }
     }
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-           CustomList<int> customList = new CustomList<int>();
-            customList.Add(1);
-            customList.Add(2);
-            customList.Add(3);
-            customList.Add(4);
-            customList.Add(5);
-            customList.Add(6);
-            
-            //removing
-            customList.Remove(2);
-            //checking if exists
-            bool f = customList.Contains(3);
-            Console.WriteLine(f);
-           
-            //removing at
-            customList.RemoveAt(1);
-            
-            //inserting at
-            customList.InsertAt(1, 100);
 
-            //clearing
-            customList.Clear();
-            customList.Print();
-        }
-    }
+
 }
---
+++ CustomList/Testing.cs
@@ -0,0 +1,33 @@
+﻿using MyCustomList;
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        CustomList<int> customList = new CustomList<int>();
+
+        customList.Add(1);
+        customList.Add(2);
+        customList.Add(3);
+        customList.Add(4);
+        customList.Add(5);
+        customList.Add(6);
+
+        //removing
+        customList.Remove(2);
+        //checking if exists
+        bool f = customList.Contains(3);
+        Console.WriteLine(f);
+
+        //removing at
+        customList.RemoveAt(1);
+
+        //inserting at
+        customList.InsertAt(1, 100);
+
+        //clearing
+        customList.Clear();
+        customList.Print();
+    }
+}
